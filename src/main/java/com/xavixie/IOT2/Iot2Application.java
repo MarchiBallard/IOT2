0ff13c055cbe145de086b41df6b97c8769119a45
xavixie <xty789455@gmail.com>
2025-03-23 20:02:50 +0800

    first commit

---
+++ src/main/java/com/xavixie/IOT2/Iot2Application.java
@@ -0,0 +1,13 @@
+package com.xavixie.IOT2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Iot2Application.class, args);
+	}
+
+}
